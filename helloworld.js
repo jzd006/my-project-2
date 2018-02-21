d0c97c4197869b2896eb7682f4b15fd207a270d1
jzd006 <31938989+jzd006@users.noreply.github.com>
2018-02-21 21:43:54 +0000

    1st commit into github

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+//different datatypes shown - string, number, booleans and null
+console.log('JavaScript')
+console.log(33.7)
+console.log(true)
+console.log(null)
+
+
+//additional attributes to datatypes
+console.log('Teaching the world how to code'.length);
+console.log('teaching the'.length)
