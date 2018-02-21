545c83d6fb670846a3e72c28e4c3430c1e3f7f7f
jzd006 <31938989+jzd006@users.noreply.github.com>
2018-02-21 22:07:35 +0000

    switcch to sandpit

---
+++ helloworld.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+//different datatypes shown - string, number, booleans and null
+console.log('JavaScript')
+console.log(33.7)
+console.log(true)
+console.log(null)
+
+
+//additional attributes to datatypes
+console.log('Teaching the world how to code'.length);
+console.log('teaching the'.length)
+
+//Methods
+console.log(Math.floor(Math.random()*100))
+//method to convert to whole number
+console.log(Math.ceil(43.8))
+//method to check if number is an integer
+console.log(Number.isInteger(2017))
+*/ covers many lin
+off a comment
+*/
